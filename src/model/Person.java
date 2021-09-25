4ae1f5465adb388882124774d420aeea07fcb7db
YanXuan1 <xuan.yan@northeastern.edu>
2021-09-26 01:59:23 +0800

    Assignment1 for 5100

---
+++ src/model/Person.java
@@ -0,0 +1,208 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author dellpc
+ */
+public class Person {
+    
+    private String name;
+    private String geographicData;
+    private String birthDate;
+    private String phoneNumber;
+    private String faxNumber;
+    private String email;
+    private String ssn;
+    private String medicalNumber;
+    private String healthNumber;
+    private String bankAccount;
+    private String certificateNumber;
+    private String vehicleIdentifier;
+    private String vehileSerialNumber;
+    private String licensePlate;
+    private String deviceIdentifier;
+    private String deviceSerialNumber;
+    private String linkedIn;
+    private String internetProtocol;
+    private String biometric;
+    
+    private String uniqueNumber;
+    private ImageIcon photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicData() {
+        return geographicData;
+    }
+
+    public void setGeographicData(String geographicData) {
+        this.geographicData = geographicData;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalNumber() {
+        return medicalNumber;
+    }
+
+    public void setMedicalNumber(String medicalNumber) {
+        this.medicalNumber = medicalNumber;
+    }
+
+    public String getHealthNumber() {
+        return healthNumber;
+    }
+
+    public void setHealthNumber(String healthNumber) {
+        this.healthNumber = healthNumber;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public String getCertificateNumber() {
+        return certificateNumber;
+    }
+
+    public void setCertificateNumber(String certificateNumber) {
+        this.certificateNumber = certificateNumber;
+    }
+
+    public String getVehicleIdentifier() {
+        return vehicleIdentifier;
+    }
+
+    public void setVehicleIdentifier(String vehicleIdentifier) {
+        this.vehicleIdentifier = vehicleIdentifier;
+    }
+
+    public String getVehileSerialNumber() {
+        return vehileSerialNumber;
+    }
+
+    public void setVehileSerialNumber(String vehileSerialNumber) {
+        this.vehileSerialNumber = vehileSerialNumber;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getDeviceIdentifier() {
+        return deviceIdentifier;
+    }
+
+    public void setDeviceIdentifier(String deviceIdentifier) {
+        this.deviceIdentifier = deviceIdentifier;
+    }
+
+    public String getDeviceSerialNumber() {
+        return deviceSerialNumber;
+    }
+
+    public void setDeviceSerialNumber(String deviceSerialNumber) {
+        this.deviceSerialNumber = deviceSerialNumber;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getInternetProtocol() {
+        return internetProtocol;
+    }
+
+    public void setInternetProtocol(String internetProtocol) {
+        this.internetProtocol = internetProtocol;
+    }
+
+    public String getBiometric() {
+        return biometric;
+    }
+
+    public void setBiometric(String biometric) {
+        this.biometric = biometric;
+    }
+
+    public ImageIcon getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(ImageIcon photo) {
+        this.photo = photo;
+    }
+
+    public String getUniqueNumber() {
+        return uniqueNumber;
+    }
+
+    public void setUniqueNumber(String uniqueNumber) {
+        this.uniqueNumber = uniqueNumber;
+    }
+    
+    
+}
